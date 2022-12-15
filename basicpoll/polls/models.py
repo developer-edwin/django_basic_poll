ccceb70c91c88dd9ccd26e793d35edf077418040
Edwin Gonzalez <developer.edwin.gonzalez@gmail.com>
2022-12-15 08:00:09 -0600

    orm

--- basicpoll/polls/models.py
+++ basicpoll/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    # id <- automatic
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("publication date")
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
