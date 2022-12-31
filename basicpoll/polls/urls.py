56aade129c18a1c786034f465b8f7dc546bfad0c
Edwin Gonzalez <developer.edwin.gonzalez@gmail.com>
2022-12-30 20:35:20 -0600

    Forms and views implemented

--- basicpoll/polls/urls.py
+++ basicpoll/polls/urls.py
@@ -6,11 +6,11 @@ app_name = "polls"
 
 urlpatterns = [
     #ie: /polls/
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     #ie: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     #ie: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
     #ie: /polls/5/vote
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
