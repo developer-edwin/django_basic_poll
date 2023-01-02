b13b1fcccef6002449c17946362f6241373cd043
Edwin Gonzalez <developer.edwin.gonzalez@gmail.com>
2023-01-02 17:50:06 -0600

    Using setUp() method

--- basicpoll/polls/tests.py
+++ basicpoll/polls/tests.py
@@ -10,10 +10,29 @@ from .models import Question
 # Create your tests here.
 class QuestionModelTests(TestCase):
     
-    def test_was_published_recently(self):
+    def setUp(self):
+        self.question = Question(question_text="Que lenguaje de programacion prefieres?")
+    
+    def test_was_published_recently_with_future_questions(self):
         """
-        test_was_published_recently This test probe the published date if was recently, including future dates that should be False
+        test_was_published_recently_with_future_questions This test probes the published date if was recently, with future questions
         """
         time = timezone.now() + datetime.timedelta(days=30)
-        the_question_test = Question(question_text="Que lenguaje de programacion prefires?", pub_date=time)
-        self.assertIs(the_question_test.was_published_recently(), False)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_present_questions(self):
+        """
+        test_was_published_recently_with_present_questions This test probes the published date if was recently, with question with less than 24 hours
+        """
+        time = timezone.now() - datetime.timedelta(hours=23)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), True)
+    
+    def test_was_published_recently_with_past_questions(self):
+        """
+        test_was_published_recently_with_past_questions This test probes the published date if was recently, with past questions
+        """
+        time = timezone.now() - datetime.timedelta(days=1, minutes=1)
+        self.question.pub_date = time
+        self.assertIs(self.question.was_published_recently(), False)
